8e0ef635df65075ed7eec1b1f350776e85e1fa7d
Diogo Filipe Faia Nunes <up202007895@fe.up.pt>
2022-10-29 15:23:46 +0000

    Replace index.sql

--- SQL/index.sql
+++ SQL/index.sql
@@ -1,13 +1,13 @@
 SET search_path TO lbaw2292;
 
 -- IDX01  It's useful for searching products of a certain product that have the price lower than a certain value defined much faster,
-CREATE INDEX product_price ON product USING btree (price);  
+CREATE INDEX product_price_idx ON product USING btree (price);  
 
 -- IDX02  Every time we need to make a query to get the address of an user, it has to be fast, because it can be executed several times
-CREATE INDEX address_iduser ON addressBook USING hash (id_user);
+CREATE INDEX address_iduser_idx ON addressBook USING hash (id_user);
 
 -- IDX03  Every time we need to make a query to get an order of any user, it has to be fast, because it can be executed several time
-CREATE INDEX order_iduser ON ord USING hash (id_user); 
+CREATE INDEX order_iduser_idx ON ord USING hash (id_user); 
 
 -- IDX11 To improve overall performance of full-text searches while searching for products by name.
-CREATE INDEX search_for_product ON product USING GIST (name); -- TODO: Isto parece-me incompleto
+CREATE INDEX search_product_name_idx ON product USING GIST (name);
