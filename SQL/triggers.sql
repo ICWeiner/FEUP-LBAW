e8374aba22a7efe7f002198df75df8f197997935
Diogo Filipe Faia Nunes <up202007895@fe.up.pt>
2022-10-31 10:44:53 +0000

    Replace triggers.sql

--- SQL/triggers.sql
+++ SQL/triggers.sql
@@ -1,15 +1,37 @@
 SET search_path TO lbaw2292;
 
--- TRIGGER06
--- This trigger will make users unable to buy more than there is available in stock.
+-- TRIGGER01
+-- This trigger will make it so a user can only make a review on a previously purchased item. (ABR.01)
+
+CREATE OR REPLACE FUNCTION check_user_ownership() RETURNS TRIGGER AS
+    $BODY$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT * 
+            FROM productOrd WHERE productOrd.id_product = New.id_product AND
+            productOrd.id_user = New.id_user) THEN
+        RAISE EXCEPTION 'User doesn''t own this product so he can''t review it.';
+       END IF;
+       RETURN NEW;
+    END
+    $BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER check_review
+        BEFORE INSERT OR UPDATE ON review
+        FOR EACH ROW
+        EXECUTE PROCEDURE check_user_ownership();
+
+-- TRIGGER02
+-- This trigger will it so a user can't buy more than the available quantity. (ABR.02)
 
 
 CREATE OR REPLACE FUNCTION check_if_enough_stock() RETURNS TRIGGER AS
     $BODY$
     BEGIN
         IF EXISTS (
-          SELECT productOrder.quantity
-          FROM productOrder, Product, Ord WHERE productOrder.id_Product = Product.id_Product AND productOrder.id_Ord = Ord.id_Ord AND productOrder.quantity > Product.quantity) THEN
+          SELECT productOrd.quantity
+          FROM productOrd, product, ord WHERE productOrd.id_product = product.id_product AND productOrd.id_ord = Ord.id_ord AND productOrd.quantity > product.quantity) THEN
            RAISE EXCEPTION 'There isn''t enough stock of products for the order.';
         END IF;
         RETURN NEW;
@@ -18,84 +40,46 @@ CREATE OR REPLACE FUNCTION check_if_enough_stock() RETURNS TRIGGER AS
 LANGUAGE plpgsql;
 
 CREATE TRIGGER check_stock
-        BEFORE INSERT OR UPDATE ON productOrder
+        BEFORE INSERT OR UPDATE ON productOrd
         FOR EACH ROW
         EXECUTE PROCEDURE check_if_enough_stock();
 
--- TRIGGER05
--- This trigger will make users unable to review items they didnt purchase.
-
-CREATE OR REPLACE FUNCTION check_user_ownership() RETURNS TRIGGER AS
-    $BODY$
-    BEGIN
-       IF NOT EXISTS (
-         SELECT *
-         FROM users, Ord, Review WHERE Ord.id_Ord = Review.id_Ord AND users.id_User = Ord.id_User) THEN
-          RAISE EXCEPTION 'User doesn''t own this product so he can''t review it.';
-       END IF;
-       RETURN NEW;
-    END
-    $BODY$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER check_review
-        BEFORE INSERT OR UPDATE ON Review
-        FOR EACH ROW
-        EXECUTE PROCEDURE check_user_ownership();
-
--- TRIGGER02
--- This trigger will make users unable to make comments or reports on their own reviews.
+-- TRIGGER03
+-- This trigger will make users unable to make comments or reports on their own reviews (BR.013)
 
 CREATE OR REPLACE FUNCTION check_own_review() RETURNS TRIGGER AS
     $BODY$
     BEGIN
-       IF EXISTS (
-         SELECT *
-         FROM users, Ord, Review WHERE Ord.id_Ord = Review.id_Ord AND users.id_User = Ord.id_User) THEN
-          RAISE EXCEPTION 'User cannot comment on its own review.';
-       END IF;
-       RETURN NEW;
+        IF EXISTS (
+            SELECT * 
+            FROM review WHERE review.id_user = New.id_user) THEN
+        RAISE EXCEPTION 'User cannot comment or report its own review.';
+        END IF;
+        RETURN NEW;
     END
     $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER check_own_review
-        BEFORE INSERT OR UPDATE ON Review
-        FOR EACH ROW
-        EXECUTE PROCEDURE check_own_review();
-
--- TRIGGER01 
--- This trigger will remove all user info from reviews, while mantaining the content itself, if that user is deleted from the database
-
-CREATE OR REPLACE FUNCTION remove_user_info() RETURNS TRIGGER AS
-    $BODY$
-    BEGIN
-           UPDATE Ord 
-           SET id_User = 1 -- set this to the "anonymousUserId"
-           WHERE "id_User" = Old."id_User";
-    END
-    $BODY$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER remove_user_info
-        BEFORE DELETE ON users
-        EXECUTE PROCEDURE remove_user_info();
+    BEFORE INSERT OR UPDATE ON review
+    FOR EACH ROW
+    EXECUTE PROCEDURE check_own_review();
 
 
 -- TRIGGER04
 
 -- Description
--- This trigger will make it so an order can have, at most, and amount of reviews equal to the amount of items (ABR.01)
+-- This trigger will make it so a user can only review a specific product once.(ABR.03)
 
 CREATE OR REPLACE FUNCTION check_review_amount() RETURNS TRIGGER AS 
     $BODY$
-    BEGIN -- can we make this better?
-        IF (
-            (SELECT count(*) FROM Ord WHERE id_User = reviewingUser) >= 
-            (SELECT count(*) FROM Review WHERE id_ord = 
-            (SELECT id_Ord FROM Ord WHERE id_User = reviewingUser))
+    BEGIN
+        IF EXISTS (
+            SELECT * 
+            FROM review WHERE review.id_product = New.id_product AND
+            review.id_user = New.id_user
             ) THEN
-            RAISE EXCEPTION 'User cannot have more reviews than purchased items'; -- TODO: this message might need to be reviewed
+            RAISE EXCEPTION 'User cannot have more reviews than purchased items'; 
         END IF;
         RETURN NEW;
     END
