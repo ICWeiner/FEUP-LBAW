3d613b9f5660c99f2194b46636bcfa54f1c3353e
Diogo Filipe Faia Nunes <up202007895@fe.up.pt>
2022-10-31 22:04:02 +0000

    Update index.sql

--- SQL/index.sql
+++ SQL/index.sql
@@ -9,5 +9,7 @@ CREATE INDEX address_iduser_idx ON addressBook USING hash (id_user);
 -- IDX03  Every time we need to make a query to get an order of any user, it has to be fast, because it can be executed several time
 CREATE INDEX order_iduser_idx ON ord USING hash (id_user); 
 
--- IDX11 To improve overall performance of full-text searches while searching for products by name.
-CREATE INDEX search_product_name_idx ON product USING GIST (name);
+-- IDX04 To improve overall performance of full-text searches while searching for products by name.
+CREATE INDEX search_product_name_idx ON product USING GIST (to_tsvector('english', name));
+
+
