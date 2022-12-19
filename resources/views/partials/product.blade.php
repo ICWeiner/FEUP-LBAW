22431dda31ab69506071b6bad6841dc5859ee0a9
Diogo Almeida <diogoalmeida.vlc@gmail.com>
2022-12-19 16:28:05 +0000

    reviews and comments user's name added on additional blades

--- resources/views/partials/product.blade.php
+++ resources/views/partials/product.blade.php
@@ -28,12 +28,13 @@
           <a> <p> {{$review->comment}} </p> </a>
           <a> <p> {{$review->rating}} </p> </a>
           <a> <p> {{$review->review_date}} </p> </a>
-          <a> <p> {{$review->reviewOwner}} </p> </a>
+          <a> <p> {{$review->reviewOwner->name}} </p> </a>
 
           <h3>Comments</h3>
           @foreach($review->comments as $comment)
             <a> <p>{{$comment->content}} </p> </a>
             <a> <p>{{$comment->rating}} </p> </a>
+            <a> <p>{{$comment->usersOwner->name}} </p> </a>
           @endforeach
         @endforeach </li>
 
