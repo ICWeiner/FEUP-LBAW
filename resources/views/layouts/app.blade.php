1d4f352d1040d8fe8225147d0f27374744869efe
root <root@Bianca>
2022-12-21 11:21:45 +0000

    more bootstrap, finihsed navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <!--<link href="{{ asset('css/layout.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script type="text/javascript">
@@ -28,88 +27,53 @@
         
         <nav class="navbar navbar-expand-lg bg-light">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="{{url('/')}}">ATGO</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
+                @if (Auth::check())
+                    <li class="van-item">
+                      <a class="nav-link active" href="{{ url('/user') }}"><span>{{ Auth::user()->name }}</span></a>
+                    </li>
+                @endif
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
+                    Categories
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><a class="dropdown-item" href="{{ url('/books') }}">Books</a></li>
+                    <li><a class="dropdown-item" href="{{ url('/funkoPops') }}">FunkoPops</a></li>
+                    <li><a class="dropdown-item" href="{{ url('/shoes') }}">Shoes</a></li>
                   </ul>
                 </li>
+                 @if (Auth::check())
                 <li class="nav-item">
-                  <a class="nav-link disabled">Disabled</a>
+                  <a class="nav-link active" href="{{ url('/cart') }}">Cart</a>
                 </li>
+                  @else
+                  <li clss="nav-item">
+                  <a class="nav-link disabled" href="{{ url('/cart') }}">Cart</a>
+                  </li>
+                @endif
+                @if (Auth::check())
+                <li class="nav-item">
+                  <a class="nav-link active" href="{{ url('/logout') }}">Logout</a>
+                </li>
+                  @else
+                  <li class="nav-item">
+                  <a class="nav-link active" href="{{ url('/login') }}">Login</a>
+                  </li>
+                @endif
               </ul>
-              <form class="d-flex" role="search">
+              <form class="d-flex" role="search" action="/search" method="get">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
               </form>
             </div>
           </div>
         </nav>
-
-
-
-
-<!--
-        <div class="hamburger-menu">
-          <input id="menu__toggle" type="checkbox" />
-          <label class="menu__btn" for="menu__toggle">
-            <span></span>
-          </label>
-
-          <ul class="menu__box">
-            <li><a class="menu__item" href="{{ url('/') }}">Home</a></li>
-            <li><a class="menuitem" href="{{ url('/shoes') }}">Shoes</a></li>
-            <li><a class="menuitem" href="{{ url('/funkoPops') }}">FunkoPops</a></li>
-            <li><a class="menu__item" href="{{ url('/books') }}">Books</a></li>
-              @if (Auth::check())
-              <li>
-                <a href="{{ url('/user') }}"><span>{{ Auth::user()->name }}</span></a>
-              </li>
-              @endif
-              @if (Auth::check())
-              <li>
-                <a class="menu_item" href="{{ url('/logout') }}">Logout</a>
-              </li>
-                @else
-                <li>
-                <a class="menu_item" href="{{ url('/login') }}">Login</a>
-                </li>
-              @endif
-            <li>
-                <a class="menu_item" href="{{ url('/cart') }}">Cart</a>
-            </li>
-          </ul>
-        </div>
--->
-        <h1><a href="{{url('/')}}">ATGO</a></h1>
-<!--
-        <div class="search">
-          <form action="/search" method="get">
-            <input type="text" name="search" placeholder="Search...">
-            <button type="submit">Go</button>
-          </form>
-        </div>
--->
-        @if (Auth::check())
-        <!--<a href="{{ url('/user') }}"><span>{{ Auth::user()->name }}</span></a><a class="button" href="{{ url('/logout') }}"> Logout </a>-->
-        @endif
       </header>
       <section id="content">
         @yield('content')
@@ -121,7 +85,101 @@
         <h6><a href="{{ url('/faq') }}">FAQ</a></h6>
         <h6><a href="{{ url('/contact') }}">Contact Us</a></h6>
       </footer>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Company name
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+
+
+
+
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     </body>
