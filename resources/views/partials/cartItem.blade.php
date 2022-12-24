df3e46d9df81c6620964210f2eb697ab5a0d3e9c
Diogo Almeida <diogoalmeida.vlc@gmail.com>
2022-12-24 17:31:11 +0000

    updated css off cartitem blade

--- resources/views/partials/cartItem.blade.php
+++ resources/views/partials/cartItem.blade.php
@@ -1,14 +1,23 @@
 <article class="cartItemPage" id="CartItem_{{$product->id_product}}">
-  <div class="cartItem">
-    <div class= "productImage">
-      <img src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_large.png?format=jpg&quality=90&v=1530129360">
-    </div>
-    <div class = "insideCartItem">
-      <h2><a href="/products/{{ $product->id_product }}">{{ $product->name }}</a></h2>
-      <h3><a href="/products/{{ $product->id_product }}">{{ $product->price }} $</a></h2>
-      <label for="quantity">quantity:</label>
-      <input type="number" id="quantity_{{ $product->id_product }}" name="quantity" value="{{ $product->pivot->quantity }}" min="0" max="{{$product->pivot->quantity}}">
-      <input type="button" value="Update Quantity" onclick="return updateCartProduct( {{$product->id_product}})">
-
-  </div>
+  <tr>
+    <td data-th="Product">
+      <div class="row">
+        <div class="col-md-3 text-left">
+          <img src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_large.png?format=jpg&quality=90&v=1530129360" alt="" class="img-fluid d-none d-md-block rounded mb-2 shadow ">
+        </div>
+        <div class="col-md-9 text-left mt-sm-2">
+          <h4>{{ $product->name }}</h4>
+        </div>
+      </div>
+    </td>
+    <td data-th="Price">{{ $product->price }} $</td>
+    <td data-th="Quantity">
+      <input type="number" class="form-control form-control-lg text-center" value="{{ $product->pivot->quantity }}">
+    </td>
+    <td class="actions" data-th="">
+      <div class="insideCartItem">
+        <input type="button" value="Update Quantity" onclick="return updateCartProduct( {{$product->id_product}})">
+      </div>
+    </td>
+  </tr>
 </article>
