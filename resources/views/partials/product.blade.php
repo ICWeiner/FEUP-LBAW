9d16bd10d26d2babc9e998f8e0359dccf826b569
Diogo Nunes <diogoffn@gmail.com>
2022-12-25 08:02:15 -0800

    bug fix involving images

--- resources/views/partials/product.blade.php
+++ resources/views/partials/product.blade.php
@@ -9,7 +9,11 @@
               <div class="col-md-6">
                 <div class="images p-3">
                   <div class="text-center p-4"> 
-                    <img id="main-image" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_large.png?format=jpg&quality=90&v=1530129360" width="250"> 
+                    @if ($product->url === null)
+                      <img id="main-image" width="250" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_large.png?format=jpg&quality=90&v=1530129360" alt="Generic placeholder image" class="img-fluid img-thumbnail mt-4 mb-2" style="width: 150px; z-index: 1">
+                    @else
+                      <img id="main-image" width="250" src="../{{ $product->url }}" alt="product image" class="img-fluid img-thumbnail mt-4 mb-2" >
+                    @endif
                   </div>
                 </div>
               </div>
