07a63174e3863a4b6dd5d5ac3baca533f265f116
Diogo Nunes <diogoffn@gmail.com>
2022-12-27 06:35:17 -0800

    html warning fixing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,17 +14,17 @@
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script type="text/javascript">
+    <script>
         // Fix for Firefox autofocus CSS bug
         // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
     </script>
-    <script type="text/javascript" src={{ asset('js/app.js') }} defer>
+    <script  src={{ asset('js/app.js') }} defer>
     </script>
-    <script type="text/javascript" src={{ asset('js/cart.js') }} defer>
+    <script src={{ asset('js/cart.js') }} defer>
     </script>
     <script src="https://kit.fontawesome.com/42e31a210b.js" crossorigin="anonymous">
     </script>
-    <script type="text/javascript" src={{ asset('js/prodPage.js') }} defer>
+    <script  src={{ asset('js/prodPage.js') }} defer>
     </script>
   </head>
   <body class="d-flex flex-column min-vh-100" style="background-color: rgba(219, 217, 211, 1);">
