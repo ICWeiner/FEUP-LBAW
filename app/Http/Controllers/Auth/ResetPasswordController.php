7853321b7a7f2db88fabf0c0160d35811524de4d
Diogo Nunes <diogoffn@gmail.com>
2023-01-02 09:17:07 -0800

    removed some comments

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -9,16 +9,7 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+
 
     use ResetsPasswords;
 
@@ -39,7 +30,7 @@ class ResetPasswordController extends Controller {
     }
 
     /**
-     * Override defaultrules
+     * Override default rules
      *
      * 
      */
